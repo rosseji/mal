1e50852abb22f4f2abae1bd6a4252cf0defcdbbf
rosseji <rosseireland@gmail.com>
2017-07-19 15:28:43 +1000

    fix sample fetch

--- R/fetch words.R
+++ R/fetch words.R
@@ -6,6 +6,11 @@ fetch_words <- function(
   path = "/TrendLock/showcase projects/turnbull/media_words.feather",
   sample = T) {
 
-  feather::read_feather(glue::glue("{fetch::fetch_local_path()}{path}"))
+  if (!sample) {
+    feather::read_feather(glue::glue("{fetch::fetch_local_path()}{path}"))
+  } else {
+    feather::read_feather(glue::glue("{fetch::fetch_local_path()}{stringr::str_replace(path, '.feather', '_sample.feather')}"))
+  }
 
 }
+
